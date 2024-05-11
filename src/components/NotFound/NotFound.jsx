a4c8b32556418e6b13be4d5ed1ad9c3eaa1bfd8c
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-05-11 20:13:37 +0300

    add styles of NotFoundPage

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,9 +4,15 @@ import styles from './notFound.module.css';
 
 export const NotFound = () => (
 	<div className={styles.notFound__container}>
-		Не корректный адрес
 		<NavLink to="/" className={styles.notFound__link}>
 			Main Page
 		</NavLink>
+		<span className={styles.notFound__404}>404 Page not found(:</span>
+		<span className={styles.notFound__text}>
+			Не корректный адрес... Попробуйте еще раз или вернитесь на{' '}
+			<NavLink to="/" className={styles.notFound__link}>
+				главную страницу
+			</NavLink>
+		</span>
 	</div>
 );
